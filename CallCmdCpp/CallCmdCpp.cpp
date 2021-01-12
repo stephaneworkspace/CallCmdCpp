0be222f778c677059413354cac60c6d330dc5643
Stéphane Bressani <s.bressani@bluewin.ch>
2021-01-12 21:26:26 +0100

    Ok c++ window process possible

--- CallCmdCpp/CallCmdCpp.cpp
+++ CallCmdCpp/CallCmdCpp.cpp
@@ -14,6 +14,13 @@
 #include <cstdint>
 #include <cinttypes>
 
+// https://stackoverflow.com/questions/5419356/redirect-stdout-stderr-to-a-string/5419409#5419409
+//#include <unistd.h>
+#include<io.h> // #include <unistd.h> https://stackoverflow.com/questions/341817/is-there-a-replacement-for-unistd-h-for-windows-visual-c
+#include <fcntl.h>
+//#include <stdio.h>
+#include <string>
+
 // CreateProcess
 #include <windows.h>
 #include <stdio.h>
@@ -35,10 +42,125 @@
     //std::cout << std::ifstream("test.txt").rdbuf();
 }*/
 
+/*
+call BeginCapture() when you need to start capture
+call EndCapture() when you need to stop capture
+call GetCapture() to retrieve captured output
+*/
 
+class StdCapture
+{
+public:
+    StdCapture(): m_capturing(false), m_init(false), m_oldStdOut(0), m_oldStdErr(0)
+    {
+        m_pipe[READ] = 0;
+        m_pipe[WRITE] = 0;
+        if (_pipe(m_pipe, 65536, O_BINARY) == -1)
+            return;
+        m_oldStdOut = _dup(_fileno(stdout));
+        m_oldStdErr = _dup(_fileno(stderr));
+        if (m_oldStdOut == -1 || m_oldStdErr == -1)
+            return;
+
+        m_init = true;
+    }
+
+    ~StdCapture()
+    {
+        if (m_capturing)
+        {
+            EndCapture();
+        }
+        if (m_oldStdOut > 0)
+            _close(m_oldStdOut);
+        if (m_oldStdErr > 0)
+            _close(m_oldStdErr);
+        if (m_pipe[READ] > 0)
+            _close(m_pipe[READ]);
+        if (m_pipe[WRITE] > 0)
+            _close(m_pipe[WRITE]);
+    }
+
+
+    void BeginCapture()
+    {
+        if (!m_init)
+            return;
+        if (m_capturing)
+            EndCapture();
+        fflush(stdout);
+        fflush(stderr);
+        _dup2(m_pipe[WRITE], _fileno(stdout));
+        _dup2(m_pipe[WRITE], _fileno(stderr));
+        m_capturing = true;
+    }
+
+    bool EndCapture()
+    {
+        if (!m_init)
+            return false;
+        if (!m_capturing)
+            return false;
+        fflush(stdout);
+        fflush(stderr);
+        _dup2(m_oldStdOut, _fileno(stdout));
+        _dup2(m_oldStdErr, _fileno(stderr));
+        m_captured.clear();
+
+        std::string buf;
+        const int bufSize = 1024;
+        buf.resize(bufSize);
+        int bytesRead = 0;
+        if (!_eof(m_pipe[READ]))
+        {
+            bytesRead = _read(m_pipe[READ], &(*buf.begin()), bufSize);
+        }
+        while(bytesRead == bufSize)
+        {
+            m_captured += buf;
+            bytesRead = 0;
+            if (!_eof(m_pipe[READ]))
+            {
+                bytesRead = _read(m_pipe[READ], &(*buf.begin()), bufSize);
+            }
+        }
+        if (bytesRead > 0)
+        {
+            buf.resize(bytesRead);
+            m_captured += buf;
+        }
+        m_capturing = false;
+        return true;
+    }
+
+    std::string GetCapture() const
+    {
+        std::string::size_type idx = m_captured.find_last_not_of("\r\n");
+        if (idx == std::string::npos)
+        {
+            return m_captured;
+        }
+        else
+        {
+            return m_captured.substr(0, idx+1);
+        }
+    }
+
+private:
+    enum PIPES { READ, WRITE };
+    int m_pipe[2];
+    int m_oldStdOut;
+    int m_oldStdErr;
+    bool m_capturing;
+    bool m_init;
+    std::string m_captured;
+};
 
 void _tmain(int argc, TCHAR * argv[])
 {
+    StdCapture std_capture;
+    std_capture.BeginCapture();
+
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
 
@@ -71,11 +193,17 @@ void _tmain(int argc, TCHAR * argv[])
 
 	// Wait until child process exits.
 	WaitForSingleObject(pi.hProcess, INFINITE);
+    std_capture.EndCapture();
+
+    std::cout << "OK" << std::endl;
+    std::cout << std_capture.GetCapture() << std::endl;
+    std::cout << "OK" << std::endl;
 
 	// Close process and thread handles. 
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
 }
+
 
 /*
 int main()
